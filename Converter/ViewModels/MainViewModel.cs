e2f3bdf3dd904f2fe1139821486493256828b365
Laurent Dehon <laurent.dehon@inforius.be>
2024-02-23 14:20:20 +0100

    Using Pdf2Image library

--- Converter/ViewModels/MainViewModel.cs
+++ Converter/ViewModels/MainViewModel.cs
@@ -1,13 +1,13 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Converter.Themes;
-using GroupDocs.Parser;
-using GroupDocs.Parser.Data;
-using GroupDocs.Parser.Options;
 using Microsoft.Win32;
+using Pdf2Image;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.IO.Compression;
+using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace Converter.ViewModels;
@@ -17,10 +17,8 @@ public partial class MainViewModel : ObservableObject
     List<string> files = [];
     [ObservableProperty] string? _theme;
     [ObservableProperty] string? __fileMessage;
-    [ObservableProperty] string? __imageMessage;
     [ObservableProperty] double _filesProgress;
-    [ObservableProperty] double _fileProgress;
-    [ObservableProperty] bool _conversion = false;
+    [ObservableProperty] bool _isFree = true;
     [ObservableProperty] System.Windows.Input.Cursor _cursor = System.Windows.Input.Cursors.Arrow;
 
     public MainViewModel()
@@ -56,21 +54,19 @@ public partial class MainViewModel : ObservableObject
     [RelayCommand]
     async Task Convert()
     {
-        if (files.Count != 0)
+        if (files.Count > 0)
         {
             Cursor = System.Windows.Input.Cursors.Wait;
-            Conversion = true;
+            IsFree = false;
             int filesCount = files.Count;
             int processedFiles = 0;
             foreach (string file in files)
             {
-                processedFiles++;
-                FilesProgress = (double)processedFiles / files.Count * 100;
-                FileMessage = $"Working on {file}";
                 await ExtractImagesAsync(file);
                 CreateArchive(file);
+                processedFiles++;
+                FilesProgress = (double)processedFiles / files.Count * 100;
             }
-            Conversion = false;
             FileMessage = $"Done converting {filesCount} file(s)";
             Cursor = System.Windows.Input.Cursors.Arrow;
         }
@@ -94,30 +90,16 @@ public partial class MainViewModel : ObservableObject
     async Task ExtractImagesAsync(string file)
     {
         string fileFolder = GetFileFolder(file);
-        FileProgress = 0;
         List<string> extractedImages = [];
         
         Directory.CreateDirectory(GetFileFolder(file));
         
         await Task.Run(() =>
         {
-            int counter = 1;
-            
-            using Parser parser = new(file);
-            IDocumentInfo info = parser.GetDocumentInfo();
-            ImageMessage = $@"Fetching images from {file}";
-            FileProgress = (double)counter / info.Pages.Count * 100;
-            IEnumerable<PageImageArea> images = parser.GetImages();
-
-            foreach (PageImageArea image in images)
-            {                
-                FileProgress = (double)counter / images.Count() * 100;
-                extractedImages.Add($@"{fileFolder}\{counter}.jpeg");
-                ImageMessage = $@"Extracting image {fileFolder}\{counter}.jpeg";
-                Image.FromStream(image.GetImageStream()).Save($@"{fileFolder}\{counter}.jpeg", System.Drawing.Imaging.ImageFormat.Jpeg);
-                counter++;
-            }
-            ImageMessage = string.Empty;
+            FileMessage = $@"Extracting images from {Path.GetFileName(file)}";
+            List<Image> images = PdfSplitter.GetImages(file, PdfSplitter.Scale.High);
+            FileMessage = $@"Writing images to disk";
+            PdfSplitter.WriteImages(file, fileFolder, PdfSplitter.Scale.High, PdfSplitter.CompressionLevel.None);
         });
     }
 
@@ -134,10 +116,8 @@ public partial class MainViewModel : ObservableObject
 
     void Reset()
     {
-        Conversion = false;
+        IsFree = true;
         FileMessage = string.Empty;
-        ImageMessage = string.Empty;
-        FileProgress = 0;
         FilesProgress = 0;
         files = [];
     }
